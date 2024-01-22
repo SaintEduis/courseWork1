6616c49c2748ebe937862bdc49e116ad81f932b4
SaintEduis <edik.melk@yandex.ru>
2024-01-22 18:51:49 +0300

    Second work commit!

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
     private int salary;
     private int id;
     private static int idCount = 0;
+
     public Employee(String fullNameOfWorker, int department, int salary) {
         this.fullNameOfWorker = fullNameOfWorker;
         this.department = department;
@@ -23,6 +24,12 @@ public class Employee {
     @Override
     public String toString() {return fullNameOfWorker + " " + department + " " + salary + " " + id;}
 
+    public static void allWorkerFullInfo() {
+        for (int i = 0; i < idCount; i++) {
+            System.out.println(Main.storageOfWorkers[i].toString());
+        }
+    }
+
     public static int costsPerMonth() {
         int salaryAmount = 0;
 
@@ -33,31 +40,130 @@ public class Employee {
         return salaryAmount;
     }
 
-    public static int minWorkerSalary() {
+    public static Employee minWorkerSalary() {
         int minSalary = Main.storageOfWorkers[0].salary;
+        int workerId = 0;
 
         for (int i = 1; i < idCount; i++) {
-            if (Main.storageOfWorkers[i].salary < minSalary) {minSalary = Main.storageOfWorkers[i].salary;}
+            if (Main.storageOfWorkers[i].salary < minSalary) {minSalary = Main.storageOfWorkers[i].salary; workerId = i;}
         }
 
-        return minSalary;
+        return Main.storageOfWorkers[workerId];
     }
 
-    public static int maxWorkerSalary() {
+    public static Employee maxWorkerSalary() {
         int maxSalary = Main.storageOfWorkers[0].salary;
+        int workerId = 0;
 
         for (int i = 1; i < idCount; i++) {
-            if (Main.storageOfWorkers[i].salary > maxSalary) {maxSalary = Main.storageOfWorkers[i].salary;}
+            if (Main.storageOfWorkers[i].salary > maxSalary) {maxSalary = Main.storageOfWorkers[i].salary; workerId = i;}
         }
 
-        return maxSalary;
+        return Main.storageOfWorkers[workerId];
     }
 
-    public static float averageSalary() {return costsPerMonth() / (idCount);}
+    public static int averageSalary() {return costsPerMonth() / (idCount);}
 
     public static void allWorkersName() {
         for (int i = 0; i < idCount; i++) {
             System.out.println(Main.storageOfWorkers[i].fullNameOfWorker);
+        }
+    }
+
+    public static void salaryIndexation(int percent) {
+        for (int i = 0; i < idCount; i++) {
+            int newSalary = (int)(Main.storageOfWorkers[i].salary + (Main.storageOfWorkers[i].salary * percent / 100));
+            Main.storageOfWorkers[i].setSalary(newSalary);
+        }
+    }
+
+    public  static Employee minWorkerSalary(int department) {
+        int minSalary = Employee.maxWorkerSalary().salary;
+        int workerId = 0;
+
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].department == department && Main.storageOfWorkers[i].salary <= minSalary) {
+                minSalary = Main.storageOfWorkers[i].salary;
+                workerId = i;
+            }
+        }
+
+        return Main.storageOfWorkers[workerId];
+    }
+
+    public  static Employee maxWorkerSalary(int department) {
+        int maxSalary = Employee.minWorkerSalary().salary;
+        int workerId = 0;
+
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].department == department && Main.storageOfWorkers[i].salary >= maxSalary) {
+                maxSalary = Main.storageOfWorkers[i].salary;
+                workerId = i;
+            }
+        }
+
+        return Main.storageOfWorkers[workerId];
+    }
+
+    public static int costsPerMonth(int department) {
+        int salaryAmount = 0;
+
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].department == department) {
+                salaryAmount += Main.storageOfWorkers[i].salary;
+            }
+        }
+
+        return salaryAmount;
+    }
+
+    public static int averageSalary(int department) {
+        int salaryAmount = 0;
+        int numbOfWorkers = 0;
+
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].department == department) {
+                salaryAmount += Main.storageOfWorkers[i].salary;
+                numbOfWorkers++;
+            }
+        }
+
+        return salaryAmount / numbOfWorkers;
+    }
+
+    public static void salaryIndexation(int department, int percent) {
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].department == department) {
+                int newSalary = (int) (Main.storageOfWorkers[i].salary + (Main.storageOfWorkers[i].salary * percent / 100));
+                Main.storageOfWorkers[i].setSalary(newSalary);
+            }
+        }
+    }
+
+    public static void allWorkerFullInfo(int department) {
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].department == department) {
+                System.out.println(Main.storageOfWorkers[i].fullNameOfWorker + " " +
+                        Main.storageOfWorkers[i].salary + " " + Main.storageOfWorkers[i].id);
+            }
+        }
+    }
+
+    public static void salaryMoreThanNumb(int numb) {
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].salary >= numb) {
+                System.out.println(Main.storageOfWorkers[i].fullNameOfWorker + " " +
+                        Main.storageOfWorkers[i].salary + " " + Main.storageOfWorkers[i].id);
+            }
+        }
+    }
+
+    public static void salaryLessThanNumb(int numb) {
+        for (int i = 0; i < idCount; i++) {
+            if (Main.storageOfWorkers[i].salary < numb) {
+                System.out.println(Main.storageOfWorkers[i].fullNameOfWorker + " " +
+                        Main.storageOfWorkers[i].salary + " " + Main.storageOfWorkers[i].id);
+            }
         }
     }
 }
